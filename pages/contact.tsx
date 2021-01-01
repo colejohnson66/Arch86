d0c2a15e7ea8c6498008fc5720282b26dc7475c3
Cole Johnson <coleharrisjohnson@gmail.com>
2021-01-01 13:41:04 -0800

    added /contact

---
+++ pages/contact.tsx
@@ -0,0 +1,47 @@
+/* This file is part of 80x86.
+ * Copyright (c) 2020 Cole Johnson
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ *   the terms of the GNU Affero General Public License as published by the Free
+ *   Software Foundation, either version 3 of the License, or (at your option)
+ *   any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ *   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *   FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ *   for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License along
+ *   with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { Breadcrumb, Col, Container, Row } from "react-bootstrap";
+
+import Layout from "../components/Layout";
+import constants from "../constants";
+
+export default () => {
+    return (
+        <Layout title="Contact">
+            <Container fluid>
+                <Breadcrumb>
+                    <Breadcrumb.Item active>Contact</Breadcrumb.Item>
+                </Breadcrumb>
+                <Row>
+                    <Col {...constants.columns.toc}>
+                        {/* No TOC */}
+                    </Col>
+                    <Col {...constants.columns.content}>
+                        <h1>Contact</h1>
+                        <p>
+                            To contact me, Cole Johnson, please use the following method:
+                        </p>
+                        <ul>
+                            <li>Email: <i>coleharrisjohnson at gmail dot com</i></li>
+                        </ul>
+                    </Col>
+                </Row>
+            </Container>
+        </Layout>
+    );
+};
