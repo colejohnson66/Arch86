63f24e83c98fe017e62b60f68fcfcd1b56be1772
Cole Johnson <coleharrisjohnson@gmail.com>
2021-01-03 19:37:52 -0800

    added generateBuildId based on git hash

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+const { promisify } = require("util");
+const exec = promisify(require("child_process").exec);
+
 module.exports = {
+    generateBuildId: async () => {
+        const { stdout } = await exec("git rev-parse HEAD");
+        return stdout.trim();
+    },
     reactStrictMode: true,
 };
