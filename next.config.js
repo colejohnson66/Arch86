8442596d11b2153be304b7c3ec67bf86dc393a5c
Cole Johnson <coleharrisjohnson@gmail.com>
2021-05-01 22:19:20 -0400

    Use webpack5

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const { promisify } = require("util");
 const exec = promisify(require("child_process").exec);
 
 module.exports = {
+    future: {
+        webpack5: true,
+    },
     generateBuildId: async () => {
         const { stdout } = await exec("git rev-parse HEAD");
         return stdout.trim();
