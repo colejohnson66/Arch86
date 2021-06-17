b3d86cdb6459b6a0d041a635686ea5db55acb49f
Cole Johnson <coleharrisjohnson@gmail.com>
2021-06-16 22:01:06 -0400

    Add `next lint` support
    Also removed `webpack5: true` as it's now enabled by default

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const { promisify } = require("util");
 const exec = promisify(require("child_process").exec);
 
 module.exports = {
-    future: {
-        webpack5: true,
-    },
     generateBuildId: async () => {
         const { stdout } = await exec("git rev-parse HEAD");
         return stdout.trim();
