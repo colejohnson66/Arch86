e7b8618fc523a615fe57d9e40250690fd63adea3
Cole Johnson <coleharrisjohnson@gmail.com>
2021-06-16 22:04:54 -0400

    Removed `generateBuildId`; unused

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
-const { promisify } = require("util");
-const exec = promisify(require("child_process").exec);
-
 module.exports = {
-    generateBuildId: async () => {
-        const { stdout } = await exec("git rev-parse HEAD");
-        return stdout.trim();
-    },
     reactStrictMode: true,
     webpack: (config, { isServer }) => {
         if (isServer)
