4802e4da3168f5ae7f91962e282198c73cfb1a88
Cole Johnson <coleharrisjohnson@gmail.com>
2022-01-09 18:01:03 -0700

    Use prebuild step instead of a hack

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    webpack: (config, { isServer }) => {
-        if (isServer) {
-            require("./scripts/GeneratePageList.js");
-        }
-        return config;
-    }
-};
